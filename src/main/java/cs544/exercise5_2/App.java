af762113334a3d37c42d417e06f28f47e3aaaae3
DUONGTRUONG-986\Duong Truong <johnangel135@gmail.com>
2018-04-21 20:51:49 -0500

    Dependency Injection with List

---
+++ src/main/java/cs544/exercise5_2/App.java
@@ -0,0 +1,16 @@
+package cs544.exercise5_2;
+
+import org.apache.xbean.spring.context.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+
+public class App
+{
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("springconfig.xml");
+		IBookService bookService = context.getBean("bookService",IBookService.class);
+		bookService.buy(new Book("123433267", "Harry Potter and the Order of the Phoenix", "J.K. Rowling"));
+        bookService.buy(new Book("888832678", "Harry Potter and the Sorcerer's Stone" , "J.K. Rowling"));
+        bookService.buy(new Book("999923156", "Harry Potter and the Goblet of Fire" ,"J.K. Rowling"));
+
+	}
+}
